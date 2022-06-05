06118281c027d900118a3fd467e1a78b0bcb81c8
nosdeedson <josemariazumira@gmail.com>
2022-06-04 21:39:19 -0300

    melhorias está dinamico heroku e local

--- client.js
+++ client.js
@@ -11,20 +11,6 @@ function consultarRestaurantes() {
     });
 }
 
-function login() {
-    const json = '{"email":"", "senha": ""}';
-    const data = JSON.parse(json);
-    $.ajax({
-        url: "http://localhost:8080/login",
-        type: "post",
-        data: JSON.stringify(data),
-        success: function (response) {
-            token = response.token;
-            $("#conteudo").text(token);
-        }
-    });
-}
-
 function endpoints() {
     // alert("teste")
     $.ajax({
@@ -37,10 +23,23 @@ function endpoints() {
     });
 }
 
-function loginHeroku() {
-    const json = '{"email":"", "senha": ""}';
+function login(email, senha) {
+    const json = `{"email": "${email}", "senha": "${senha}"}`;
     const data = JSON.parse(json);
-        
+    $.ajax({
+        url: "http://localhost:8080/login",
+        type: "post",
+        data: JSON.stringify(data),
+        success: function (response) {
+            token = response.token;
+            $("#conteudo").text(token);
+        }
+    });
+}
+
+function loginHeroku( email, senha) {
+    const json = `{"email": "${email}", "senha": "${senha}" }`;
+    const data = JSON.parse(json);
     $.ajax({
         url: "https://ejs-algafood.herokuapp.com/login",
         type: "post",
@@ -54,17 +53,43 @@ function loginHeroku() {
 
 }
 
+let requestTo = ''
+let requestHeroku = false;
+
+function limpar(){
+    requestHeroku = false
+    requestTo = ''
+    $('#email').val('');
+    $('#senha').val('');
+    $("#heroku").prop("checked").checked = false;
+}
+
 function listaUsuarios() {
+    alert(requestTo)
     $.ajax({
-        url: "https://ejs-algafood.herokuapp.com/usuarios",
+        url: requestTo,
         type: "get",
         headers: {"Authorization": "Bearer " + token},
         success: function (response) {
             $("#conteudo").text(JSON.stringify(response._embedded.usuários));
+            limpar()
         }
     });
 }
 
-
-$('#botao').click(loginHeroku)
+$('#botao').click(function() {
+    const email = $('#email').val();
+    const senha = $('#senha').val();
+    requestHeroku = $("#heroku").prop('checked')
+    if(requestHeroku){
+        alert("heroku")
+        requestTo = 'https://ejs-algafood.herokuapp.com/usuarios'
+        loginHeroku(email, senha)
+    }else{
+        alert('local')
+        requestTo = 'http://localhost:8080/usuarios'
+        login(email, senha);
+    }
+    
+})
 $('#botaoUsuario').click(listaUsuarios)
